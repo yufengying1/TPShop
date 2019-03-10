0203f24eec61208eb85d656a3d499369cbec4b37
yufengying1 <1531335503@qq.com>
2019-03-10 22:03:19 +0800

    跳转到登录界面

---
+++ page/page.py
@@ -0,0 +1,18 @@
+from base.base_driver import init_driver
+from page.home_page import HomePage
+from page.mine_page import MinePage
+
+
+class Page:
+    def __init__(self,driver):
+        self.driver = driver
+
+    @property
+    def home(self):
+        return HomePage(self.driver)
+
+    @property
+    def mine(self):
+        return MinePage(self.driver)
+
+
