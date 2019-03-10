c4f60f1b78a46ab6ca365c80125739019c8902f8
yufengying1 <1531335503@qq.com>
2019-03-10 19:40:21 +0800

    启动应用程序

--- scripts/test_login.py
+++ scripts/test_login.py
@@ -1,3 +1,15 @@
+import time
+
+from base.base_driver import init_driver
+
+
 class TestLogin:
+    def setup(self):
+        self.driver = init_driver()
+
+    def teardown(self):
+        time.sleep(1)
+        self.driver.quit()
+
     def test_login(self):
         print("1")
